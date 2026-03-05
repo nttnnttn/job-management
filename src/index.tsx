b03f662ede2904e2d06d7eb387bc5f29472030e7
Le Viet Vu <levietvu1995@gmail.com>
2026-03-05 14:02:33 +0700

    Update index.tsx
    initial client api

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";   // ← BẮT BUỘC để Tailwind chạy
+import { setupApiClient } from "./api-client/setup-client";
+
+setupApiClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
