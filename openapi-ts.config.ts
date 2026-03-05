97cf0a3c96709c9ac164a2ddcf2565393b358cdd
Le Viet Vu <levietvu1995@gmail.com>
2026-03-05 10:49:43 +0700

    Create openapi-ts.config.ts
    create new config file

---
+++ openapi-ts.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+import dotenv from 'dotenv';
+
+// Load variables from .env
+dotenv.config();
+
+export default defineConfig({
+  input: `${process.env.REACT_APP_API_BASE}/swagger/json`, // Path to your NestJS file
+  output: './src/api-client',           // Where the generated code will go
+  plugins: [
+    '@hey-api/client-axios',            // Explicitly enable Axios support
+  ],
+});
