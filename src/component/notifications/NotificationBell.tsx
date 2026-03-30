2c223965de7513030a3c3c504b4ce010d05185fe
Le Viet Vu <levietvu1995@gmail.com>
2026-03-30 15:09:21 +0700

    Update NotificationBell.tsx

--- src/component/notifications/NotificationBell.tsx
+++ src/component/notifications/NotificationBell.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useNotifications } from '../../hooks/useNotifications';
+import { useNotifications } from '../../contexts/useNotifications';
 import { NotificationDropdown } from './NotificationDropdown';
 import './notifications.css';
 
